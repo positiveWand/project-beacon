4ccbbb136aa477a59244f41fccd58294160b8f20
positiveWand <singbhang@gmail.com>
2023-05-23 13:58:44 +0900

    MainPage 날씨 출력 프로토타입

--- frontend/src/mainPage/MainPage.jsx
+++ frontend/src/mainPage/MainPage.jsx
@@ -1,35 +1,7 @@
-import React, { useEffect, useState } from 'react';
 import Header from '../searchPage/Header';
+import WeatherBox from './WeatherBox';
 
 export default function MainPage() {
-  const [weather, setWeather] = useState(null);
-
-  useEffect(() => {
-    const fetchWeather = async () => {
-      const apiKey = 'fb12024bc85a368adf85c553d153df91';
-      const city = 'Seoul';
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-
-      try {
-        const response = await fetch(url);
-        const data = await response.json();
-        if (response.ok) {
-          setWeather(data);
-        } else {
-          console.log('날씨 정보를 가져오는 데 실패했습니다:', data.message);
-        }
-      } catch (error) {
-        console.log('날씨 정보를 가져오는 데 실패했습니다:', error);
-      }
-    };
-
-    fetchWeather();
-  }, []);
-
-  if (!weather) {
-    return <div>Loading...</div>;
-  }
-
   const navTarget = [
       {name: "홈", to: "/src/mainPage/", active: true},
       {name: "탐색", to: "/src/searchPage/", active: false},
@@ -47,10 +19,8 @@ export default function MainPage() {
   return (
       <div className="d-flex flex-column overflow-y-auto">
           <Header title="B.M.S" navTargets={navTarget} toLoginHandler={handleToLoginButton} toSignupHandler={handleToSignupButton} />
-          <div>
-              <h2>현재 날씨: {weather.weather[0]?.main}</h2> 
-              <p>온도: {Math.round(weather.main?.temp - 273.15)}°C</p> 
-              <p>습도: {weather.main?.humidity}%</p> 
+          <div className="d-flex flex-column">
+              <WeatherBox styleClass={["m-3", "p-3","border", "border-2", "rounded-2"]}/>
           </div>
       </div>
   );
---
+++ frontend/src/mainPage/WeatherBox.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+
+export default function WeatherBox({styleClass}) {
+    const [weather, setWeather] = useState(null);
+  
+    useEffect(() => {
+      const fetchWeather = async () => {
+        const apiKey = 'fb12024bc85a368adf85c553d153df91';
+        const city = 'Daejeon';
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=kr`;
+  
+        try {
+          const response = await fetch(url);
+          const data = await response.json();
+          if (response.ok) {
+            setWeather(data);
+          } else {
+            console.log('날씨 정보를 가져오는 데 실패했습니다:', data.message);
+          }
+        } catch (error) {
+          console.log('날씨 정보를 가져오는 데 실패했습니다:', error);
+        }
+      };
+  
+      fetchWeather();
+    }, []);
+  
+    return (
+        <div className={["d-flex", "justify-content-center"].concat(styleClass).join(' ')}>
+            <div className="d-flex flex-column align-items-center">
+                <div className="d-flex align-items-center">
+                    <img src="/src/assets/black_marker.png" alt="마커" height={20} width={20}/>
+                    <span className="ms-1">{(weather ? weather.name : "") + " 날씨"}</span>
+                </div>
+                <h2>날씨: {weather ? weather.weather[0].main : "-"}</h2>
+                <img src={weather ? "https://openweathermap.org/img/wn/"+weather.weather[0].icon+"@2x.png" : ""} alt="날씨 아이콘" />
+                <p className="m-0">온도: {weather ? Math.round(weather.main.temp - 273.15) : "-"}°C</p> 
+                <p className="m-0">습도: {weather ? weather.main.humidity : "-"}%</p> 
+            </div>
+        </div>
+    );
+  }
